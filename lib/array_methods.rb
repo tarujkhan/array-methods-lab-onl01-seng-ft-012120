a717a2117084376892a35351def765e5034de268
taroj khan <tarujkhan@yahoo.com>
2020-01-26 06:26:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,29 +1,29 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.include?(array)
+array.include?(element)
 end
 
 def using_sort(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.sort(array)
+array.sort(element)
 end
 
 def using_reverse(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.reverse(array)
+array.reverse(element)
 end
 
 def using_first(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.first(array)
+array.first(element)
 end
 
 def using_last(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.last(array)
+array.last(element)
 end
 
 def using_size(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.size(array)
+array.size(element)
 end
